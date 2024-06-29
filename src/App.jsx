1723943c2b53442b583aeddf26a6b6fa604fe2e4
SJ_0741 <112848126+Shadan-Jamal@users.noreply.github.com>
2024-06-29 20:51:05 +0530

    add template structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react';
+import Font, {Text} from 'react-font';
 
 function App() {
+  const [inputValue,setInputValue] = useState('');
+  
   return (
-    <div></div>
+    <>
+    <div className='grid place-content-center gap-9 h-screen'>
+    <Font family='Bebas Neue'>
+      <input
+      value={inputValue}
+      onChange={(e) => setInputValue(e.target.value)}
+      className='w-[900px] text-3xl text-white px-5 py-4 bg-transparent border border-white rounded-full' 
+      type="text" />
+      </Font>
+      <Text className='bg-transparent tracking-widest max-w-[900px] max-h-[400px] overflow-y-scroll text-wrap p-5 rounded-lg'>
+        
+      </Text>
+      </div>
+    </>
   )
 }
 
