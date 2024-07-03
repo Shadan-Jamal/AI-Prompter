5631546abcc9bd4e9985268f13db589811e36410
SJ_0741 <112848126+Shadan-Jamal@users.noreply.github.com>
2024-07-03 20:11:44 +0530

    add loading state

--- src/App.jsx
+++ src/App.jsx
@@ -14,49 +14,56 @@ function App() {
 
   const handleKeyDown = (e) => {
     if (e.code === "Enter") {
+      setLoading(true);
       setPrompt(() => inputValue)
     }
   }
 
   useEffect(() => {
 
+    // console.log(`before fetch ${loading}`);
+    // setLoading(true);
     const fetchData = async () => {
-      // prompt.length > 0 ? setLoading(true) : setLoading(false);
-      console.log(prompt);
-
+      // console.log(`after fetch ${loading}`)
       if(prompt.length > 0 && inputValue.length > 0){
+        // console.log(`inside if ${loading}`)
         const result = await model.generateContent(prompt);
         const response = await result.response;
-        // console.log(response.text());
         setOutput(() => {
           const text = response.text();
-          return text.split('')
+          setLoading(false);
+          console.log(text,loading);
+          return text;
         });
       }
     }
-    console.log(output);
     fetchData();
 
-  },[prompt,setPrompt])
+  },[prompt,setPrompt,loading])
 
 
   return (
     <>
     <Navbar />
-    <div className='w-[70vw] leading-7 h-screen p-10'>
+    <div className='w-[90vw] leading-7 h-screen p-10'>
     <Font family='Dosis' weight={700}>
       <h1 className='text-4xl font-bold text-start tracking-widest text-white p-5'>Enter A Prompt. </h1>
-      <input
-      value={inputValue}
-      onChange={(e) => setInputValue(e.target.value)}
-      onKeyDown={(e) => handleKeyDown(e)}
-      className='w-3/4 text-[20px] text-white px-3 py-3 bg-transparent border border-white rounded-full' 
-      type="text" />
-      </Font>
-      <Text family='Dosis' className='bg-transparent tracking-[2.4px] max-w-full max-h-[200px] text-wrap p-5 rounded-lg text-[15px]'>
-        {output}
-      </Text>
+      <div className='mb-3'>
+        <input
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+        onKeyDown={(e) => handleKeyDown(e)}
+        className='w-3/4 text-[20px] text-white px-3 py-3 bg-transparent border-b-2 border-b-white focus:outline-none' 
+        type="text" />
       </div>
+      </Font>
+      {loading ?
+      <Text family='Dosis' className='bg-transparent tracking-[2.4px]text-[15px] px-4 py-2 font-light'> Loading...</Text>
+      : <Text family='Dosis' className='bg-transparent tracking-[2.4px] text-wrap p-5 rounded-lg text-[15px] whitespace-pre-wrap px-4 py-2 border-l-2 border-l-slate-300 overflow-scroll font-light'>
+      {output}
+      </Text>
+      }
+    </div>
     </>
   )
 }
